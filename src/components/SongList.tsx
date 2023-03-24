98e322c981486beb96777d99103ceb07d7b0a876
Muhamad Faishal Irawan <muhammadfaishal193@gmail.com>
2023-03-24 09:08:01 +0700

    songlist and item props type inference

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { api } from "~/utils/api";
+import { api, type RouterOutputs } from "~/utils/api";
 
 const SongList = () => {
   const { data: songData } = api.song.getAll.useQuery();
@@ -10,47 +10,30 @@ const SongList = () => {
       <h1>Trending</h1>
       <div className="flex gap-6 overflow-x-scroll whitespace-nowrap">
         {songData?.map((song, i) => (
-          <SongItem
-            songId={song.id}
-            artistId={song.artist.id}
-            key={i}
-            imgSrc={song.cover}
-            artist={song.artist.name}
-            title={song.title}
-          />
+          <SongItem {...song} key={i} />
         ))}
       </div>
     </div>
   );
 };
 
-type SongItemProps = {
-  artistId: string;
-  songId: string;
-  imgSrc: string;
-  title: string;
-  artist: string;
-};
+type SongItemProps = RouterOutputs["song"]["getAll"][number];
 
-const SongItem = ({
-  songId,
-  artistId,
-  imgSrc,
-  title,
-  artist,
-}: SongItemProps) => {
+const SongItem = (props: SongItemProps) => {
+  const { id, artist, cover, title } = props;
+  console.log(cover);
   return (
     <div className="flex flex-col">
-      <Link href={`/song/${songId}`}>
+      <Link href={`/song/${id}`}>
         <Image
-          src={imgSrc}
-          alt={`${title} - ${artist} Album Cover`}
+          src={cover}
+          alt={`${title} - ${artist.name} Album Cover`}
           width={150}
           height={150}
         />
         <h1>{title}</h1>
       </Link>
-      <Link href={`/artist/${artistId}`}>{artist}</Link>
+      <Link href={`/artist/${artist.id}`}>{artist.name}</Link>
     </div>
   );
 };
