42d6dcd8e11cd2555cbba4fc49f224588065b7bd
Muhamad Faishal Irawan <muhammadfaishal193@gmail.com>
2023-03-26 16:59:10 +0700

    undo seperation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,56 @@
 import { type NextPage } from "next";
-
-import SongList from "~/components/SongList";
 import Layout from "~/components/Layout";
+
+import Image from "next/image";
+import Link from "next/link";
+import { api, type RouterOutputs } from "~/utils/api";
+
+const SongList = () => {
+  const { data: songData, isLoading } = api.song.getAll.useQuery();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!songData) {
+    return <div>Something went wrong</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="mb-6 text-4xl font-bold">Trending</h1>
+      <div className="flex items-center gap-6 overflow-x-scroll whitespace-nowrap pb-8">
+        {songData.map((song, i) => (
+          <SongItem {...song} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+type SongItemProps = RouterOutputs["song"]["getAll"][number];
+
+const SongItem = (props: SongItemProps) => {
+  const { id, artist, cover, title } = props;
+
+  return (
+    <div className="flex min-w-fit flex-col">
+      <Link href={`/song/${id}`}>
+        <Image
+          className="mb-2 rounded-lg"
+          src={cover}
+          alt={`${title} - ${artist.name} Album Cover`}
+          width={226}
+          height={226}
+        />
+        <h1 className="text-lg font-medium">{title}</h1>
+      </Link>
+      <Link href={`/artist/${artist.id}`} className="text-white/70">
+        {artist.name}
+      </Link>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   return (
--- src/pages/song/[songId].tsx
+++ src/pages/song/[songId].tsx
@@ -9,7 +9,6 @@ import {
   type Dispatch,
   type SetStateAction,
 } from "react";
-import LanguageToggle from "~/components/LanguageToggle";
 import Layout from "~/components/Layout";
 import { api } from "~/utils/api";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
@@ -30,6 +29,75 @@ import * as Slider from "@radix-ui/react-slider";
 import ReactPlayer from "react-player/youtube";
 import * as Toggle from "@radix-ui/react-toggle";
 import { motion } from "framer-motion";
+
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
+
+const LanguageToggle = ({
+  langs,
+  activeLangs,
+  setActiveLangs,
+  karaokeMode,
+}: {
+  langs: string[];
+  activeLangs: string[];
+  setActiveLangs: (value: string[]) => void;
+  karaokeMode: boolean;
+}) => {
+  if (karaokeMode) {
+    return (
+      <ToggleGroup.Root
+        className="inline-flex space-x-px rounded "
+        type="single"
+        value={activeLangs[0]}
+        onValueChange={(value) => {
+          // Ensure at least one toggle is active
+          if (value) {
+            setActiveLangs([value]);
+          }
+        }}
+        aria-label="Language"
+      >
+        {langs.map((lang, i) => (
+          <ToggleGroup.Item
+            className="flex h-[35px] w-[60px] items-center justify-center bg-[#1d1d1d] text-base leading-4 text-white first:rounded-l last:rounded-r hover:bg-[#353535] focus:z-10 focus:outline-none data-[state=on]:bg-white data-[state=on]:text-black"
+            key={i}
+            value={lang}
+            aria-label={lang}
+          >
+            {lang}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
+    );
+  } else {
+    return (
+      <ToggleGroup.Root
+        className="inline-flex space-x-px rounded "
+        type={"multiple"}
+        value={activeLangs}
+        onValueChange={(value) => {
+          // Ensure at least one toggle is active
+          console.log(value);
+          if (value && value.length !== 0 && value.length <= 3) {
+            setActiveLangs(value);
+          }
+        }}
+        aria-label="Language"
+      >
+        {langs.map((lang, i) => (
+          <ToggleGroup.Item
+            className="flex h-[35px] w-[60px] items-center justify-center bg-[#1d1d1d] text-base leading-4 text-white first:rounded-l last:rounded-r hover:bg-[#353535] focus:z-10 focus:outline-none data-[state=on]:bg-white data-[state=on]:text-black"
+            key={i}
+            value={lang}
+            aria-label={lang}
+          >
+            {lang}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
+    );
+  }
+};
 
 const PlayBar = ({
   activeLangs,
