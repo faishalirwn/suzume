f6741810b99336226f5f0a236e584ff1d110da27
Muhamad Faishal Irawan <muhammadfaishal193@gmail.com>
2023-10-29 15:45:03 +0700

    Initial commit, complete rewrite

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <main>
+      <nav>
+        <p>logo</p>
+        <div>search</div>
+      </nav>
+      <div>
+        <h1>Trending</h1>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>title</p>
+            <p>artist</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>title</p>
+            <p>artist</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center">
-      <p>Made by udung the great</p>
-    </footer>
-  );
-};
-
-export default Footer;
